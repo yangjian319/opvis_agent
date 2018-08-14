3398b71ec27c49a7e229adacad549bca312a10c6
yangjian319 <yangjian319@gmial.com>
2018-08-14 14:01:11 +0800

    更改文件名

--- agent_service/update/agentupdate.sh
+++ agent_service/update/agentupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date=`date +%Y%m%d`
-mv /home/opvis/opvis_agent/agent_service/agent.py /home/opvis/opvis_agent/agent_service/temp/agent.py.$date
-cp /home/opvis/opvis_agent/agent_service/temp/agent.py /home/opvis/opvis_agent/agent_service/
-ps aux|grep agent.py|grep -v grep|awk '{print $2}'|xargs kill -9
-python /home/opvis/opvis_agent/agent_service/agent.py
+mv /home/opvis/opvis_agent/agent_service/agent_udp.py /home/opvis/opvis_agent/agent_service/temp/agent_udp.py.$date
+cp /home/opvis/opvis_agent/agent_service/temp/agent_udp.py /home/opvis/opvis_agent/agent_service/
+ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
+python /home/opvis/opvis_agent/agent_service/agent_udp.py
