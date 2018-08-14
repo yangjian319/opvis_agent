013a76a99571c81b4c8de6120d686512587f1a4a
yangjian319 <yangjian319@gmial.com>
2018-08-14 14:27:11 +0800

    更改脚本

--- agent_service/restart.sh
+++ agent_service/restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps aux|grep agent.py|grep -v grep|awk '{print $2}'|xargs kill -9
-python agent.py
+ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
+python agent_udp.py
