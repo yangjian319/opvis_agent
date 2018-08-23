662b33a9ab5572aa6e06f3b3d05cc8059d3a1818
yangjian319 <yangjian319@gmial.com>
2018-08-23 09:33:03 +0800

    更改日志路径

--- agent_service/restart.sh
+++ agent_service/restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
-python agent_udp.py
+/usr/bin/nohup /usr/bin/python -u /home/opvis/opvis_agent/agent_service/agent_udp.py > /home/opvis/opvis_agent/agent_service/log/agent.log 2>&1 &
