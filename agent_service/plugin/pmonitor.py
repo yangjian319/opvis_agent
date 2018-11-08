0644020dddd7ff06844e1c2af7bee002111370be
yangjian319 <yangjian319@gmial.com>
2018-11-08 16:32:58 +0800

    修改key_word引号

--- agent_service/plugin/pmonitor.py
+++ agent_service/plugin/pmonitor.py
@@ -8,13 +8,14 @@ import os
 import sys
 import json
 import urllib
+import urllib2
 import logging
 import datetime
 from logging.handlers import TimedRotatingFileHandler
 
 
 # log
-LOG_FILE = "/home/opvis/opvis_agent/agent_service/log/pmonitor.log"
+LOG_FILE = "/home/opvis/utils/log/pmonitor.log"
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 fh = TimedRotatingFileHandler(LOG_FILE, when='D', interval=1, backupCount=30)
@@ -24,46 +25,98 @@ formatter = logging.Formatter(format_str, datefmt)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-if not os.path.exists("/data/py/test"):
-  os.mkdir("/home/opvis/opvis_agent/agent_service/pm")
+arg = sys.argv[1]
+arg_number = arg.split("=")[1].replace("\n","")[-2:-1]
+logging.info(str(arg_number))
+arg_time = arg.split("=")[1].replace("\n","")[-1:]
+logging.info(str(arg_time))
 
-arg_cycle = sys.argv[1]
-allitems = "/home/opvis/opvis_agent/agent_service/pm/allitems"
-localip = os.popen("ifconfig eth0|grep 'inet'|awk 'NR==1 {print $2}'").read().replace("\n", "")
+allitems = "/home/opvis/utils/pm/allitems"
+def fun():
+  lm=[]
+  lh = []
+  with open(allitems,"r") as fd:
+    for i in fd.readlines():
+      j = json.loads(i)
+      if arg_time == "m" and int(j["trigger_cycle_unit"]) == 0 and int(j["trigger_cycle_value"]) == int(arg_number):
+        lm.append(j)
+      else:
+        if int(j["trigger_cycle_unit"]) != 0 and int(j["trigger_cycle_value"]) == int(arg_number):
+          lh.append(j)
+  if lm and arg_time == "m":
+    check_process(lm)
+  if lh and arg_time == "h":
+    check_process(lh)
 
-fd = open(allitems,"r")
-line = json.loads(fd.readline())
-while line:
-  key_word = line.get("key_word")
-  IP = line.get("IP")
-  process_name = line.get("process_name")
-  trigger_cycle_value = line.get("trigger_cycle_value")
-  trigger_value = line.get("trigger_value")
-  need_monitor = []
-  cmd = "grep " +  arg_cycle + " " + allitems + '|awk -F \',\' \'{print $2}\'|awk -F \'=\' \'{print $2}\''
-  process = os.popen(cmd).readlines()
-  for i in process:
-    need_monitor.append(i.replace("\n",""))
-  for p in need_monitor and p == key_word:
-    count_new = os.popen("ps aux|grep %s|grep -v grep|wc -l" %p).readline()[0]
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+def check_process(ll):
+  with open("/home/opvis/utils/agent.lock", "r") as fd:
+    proxy_ip = fd.readline().split(":")[0]
+  get_process_url = "http://" + proxy_ip + ":9995" + "/storeinfo/"
+  for x in ll:
+    id = x.get("id")
+    biz_ip = x.get("biz_ip")
+    manage_ip = x.get("manage_ip")
+    process_name = x.get("process_name")
+    key_word = x.get("key_word")
+    key_word = "'" + key_word + "'"
+    trigger_compare = x.get("trigger_compare")
+    trigger_value = x.get("trigger_value")
+    trigger_level = x.get("trigger_level")
+    trigger_cycle_value = x.get("trigger_cycle_value")
+    trigger_cycle_unit = x.get("trigger_cycle_unit")
+    count_new = os.popen("ps aux|grep %s|grep -v grep|wc -l" % key_word).readline()[0]
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     if int(count_new) < int(trigger_value):
-      # 记录日志
-      content = process_name + " " + "老的进程数是：" + trigger_value + " 新的进程数是：" + count_new
-      logging.info(content)
-      upload_data = {}
-      upload_data["process_name"] = process_name
-      upload_data["old_count"] = trigger_value
-      upload_data["new_count"] = count_new
-      upload_data["current_time"] = current_time
-      upload_data["localip"] = localip
-      urllib.urlencode(upload_data)
-      with open("/home/opvis/opvis_agent/agent_service/agent.lock", "r") as fd:
-        proxy_ip = fd.readline()
-      get_process_url = "http://" + proxy_ip + ":9995" + "/getinfo/"
-      req = urllib2.Request(url=get_process_url, data=upload_data)
-      res = urllib2.urlopen(req)
-      get_data = res.read()
-      logging.info("process monitor sucess")
-    line = fd.readline()
-fd.close()
+      try:
+        upload_data = {}
+        upload_data["id"] = id
+        upload_data["biz_ip"] = biz_ip
+        upload_data["manage_ip"] = manage_ip
+        upload_data["process_name"] = process_name
+        upload_data["key_word"] = key_word
+        upload_data["trigger_compare"] = trigger_compare
+        upload_data["trigger_value"] = trigger_value
+        upload_data["trigger_level"] = trigger_level
+        upload_data["trigger_cycle_value"] = trigger_cycle_value
+        upload_data["trigger_cycle_unit"] = trigger_cycle_unit
+        upload_data["should_be"] = trigger_value
+        upload_data["new_count"] = count_new
+        upload_data["current_time"] = current_time
+        upload_data = urllib.urlencode(upload_data)
+        req = urllib2.Request(url=get_process_url, data=upload_data)
+        res = urllib2.urlopen(req)
+        get_data = res.read()
+        if get_data == "ok":
+          logging.info("process is less than original!" + " process name is: " + str(key_word) + " " + " machine ip is: " + str(biz_ip))
+        else:
+          logging.info(get_data)
+      except Exception as e:
+        logging.info("Storeinfo error. " + str(e))
+    else:
+      try:
+        upload_data = {}
+        upload_data["id"] = id
+        upload_data["biz_ip"] = biz_ip
+        upload_data["manage_ip"] = manage_ip
+        upload_data["process_name"] = process_name
+        upload_data["key_word"] = key_word
+        upload_data["trigger_compare"] = trigger_compare
+        upload_data["trigger_value"] = trigger_value
+        upload_data["trigger_level"] = trigger_level
+        upload_data["trigger_cycle_value"] = trigger_cycle_value
+        upload_data["trigger_cycle_unit"] = trigger_cycle_unit
+        upload_data["should_be"] = trigger_value
+        upload_data["new_count"] = count_new
+        upload_data["current_time"] = current_time
+        upload_data = urllib.urlencode(upload_data)
+        req = urllib2.Request(url=get_process_url, data=upload_data)
+        res = urllib2.urlopen(req,timeout=70)
+        get_data = res.read()
+        if get_data == "ok":
+          logging.info("process monitor is ok！" + " process name is: " + str(key_word) + " " + " machine ip is: " + str(biz_ip))
+        else:
+          logging.info(get_data)
+      except Exception as e:
+        logging.info("Storeinfo error. " + str(e))
+
+check_process()
