6050062788aa983edf19c251cc08ca288e3d4cfd
yangjian319 <yangjian319@gmial.com>
2018-11-08 16:57:29 +0800

    修改路径

--- agent_service/update/agentupdate.sh
+++ agent_service/update/agentupdate.sh
@@ -4,4 +4,4 @@ ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
 rm -rf  /home/opvis/opvis_agent
 tar zxf /home/opvis/opvis_agent.tar.gz -C /home/opvis/
 mv /home/opvis/opvis_agent.tar.gz /home/opvis/opvis_agent.tar.gz.$time
-python /home/opvis/utils/agent_udp.py
+python /home/opvis/opvis_agent/agent_service/agent_udp.py
