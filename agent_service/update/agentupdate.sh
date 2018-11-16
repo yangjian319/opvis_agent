df87ec81d1acf7dff6d15612acac6a413b4d3452
yangjian319 <yangjian319@gmial.com>
2018-11-16 09:23:14 +0800

    修改重命名

--- agent_service/update/agentupdate.sh
+++ agent_service/update/agentupdate.sh
@@ -3,5 +3,5 @@ time=`date +%Y%m%d%H%M%S`
 ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
 rm -rf  /home/opvis/opvis_agent
 tar zxf /home/opvis/opvis_agent.tar.gz -C /home/opvis/
-mv /home/opvis/opvis_agent.tar.gz /home/opvis/opvis_agent.tar.gz.$time
+mv /home/opvis/opvis_agent.tar.gz.bak /home/opvis/opvis_agent.tar.gz.bak.$time
 python /home/opvis/opvis_agent/agent_service/agent_udp.py
