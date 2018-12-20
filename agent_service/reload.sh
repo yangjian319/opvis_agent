3cefdb5eba3529af0313c6274c808866a5fc0e76
yangjian319 <yangjian319@gmial.com>
2018-12-20 15:53:54 +0800

    日志追加

--- agent_service/reload.sh
+++ agent_service/reload.sh
@@ -1,2 +1,2 @@
 ps -ef | grep -v grep | grep agent_udp.py| awk '{print $2}'|xargs kill -9
-/usr/bin/nohup /usr/bin/python -u /home/opvis/opvis_agent/agent_service/agent_udp.py > /home/opvis/opvis_agent/agent_service/log/agent.log 2>&1 &
+/usr/bin/nohup /usr/bin/python -u /home/opvis/opvis_agent/agent_service/agent_udp.py >> /home/opvis/opvis_agent/agent_service/log/agent.log 2>&1 &
