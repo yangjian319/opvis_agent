5153f355c801e42e3b3d255e794d478cb60e6609
yangjian319 <yangjian319@gmial.com>
2019-01-16 09:21:14 +0800

    打包后重启方式

--- agent_service/restart.sh
+++ agent_service/restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
-/usr/bin/nohup /usr/bin/python -u /home/opvis/opvis_agent/agent_service/agent_udp.py >> /home/opvis/utils/log/agent.log 2>&1 &
+/usr/bin/nohup /home/opvis/opvis_agent/agent_service/agent_udp >> /home/opvis/utils/log/agent.log 2>&1 &
