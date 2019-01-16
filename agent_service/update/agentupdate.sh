3fac7b4f6c96e6ae969a67ceaa72fdfefb4bb8b3
yangjian319 <yangjian319@gmial.com>
2019-01-16 09:23:14 +0800

    修改升级脚本，增加了对版本的处理

--- agent_service/update/agentupdate.sh
+++ agent_service/update/agentupdate.sh
@@ -2,6 +2,7 @@
 time=`date +%Y%m%d%H%M%S`
 ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
 rm -rf  /home/opvis/opvis_agent
-tar zxf /home/opvis/opvis_agent.tar.gz -C /home/opvis/
-mv /home/opvis/opvis_agent.tar.gz.bak /home/opvis/opvis_agent.tar.gz.bak.$time
-python /home/opvis/opvis_agent/agent_service/agent_udp.py
+tar zxf /home/opvis/opvis_agent_v*.tar.gz -C /home/opvis/
+mv /home/opvis/opvis_agent_v*.tar.gz /home/opvis/opvis_agent_v*.tar.gz.$time
+cd /home/opvis/opvis_agent/agent_service
+./start.sh
