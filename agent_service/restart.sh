b030c5ffd891a521c56f717ad943183142ed55ab
yangjian319 <yangjian319@gmial.com>
2019-01-25 10:29:42 +0800

    去掉.py

--- agent_service/restart.sh
+++ agent_service/restart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
+ps aux|grep agent_udp|grep -v grep|awk '{print $2}'|xargs kill -9
 /usr/bin/nohup /home/opvis/opvis_agent/agent_service/agent_udp >> /home/opvis/utils/log/agent.log 2>&1 &
