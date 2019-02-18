62caf83aa0772429dbde77a5bb38d3497392f9db
yangjian319 <yangjian319@gmial.com>
2019-02-18 12:16:18 +0800

    添加判断脚本是否存在。

--- agent_service/plugin/settled_monitor.py
+++ agent_service/plugin/settled_monitor.py
@@ -17,7 +17,7 @@ from logging.handlers import TimedRotatingFileHandler
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-LOG_FILE = "/home/opvis/utils/log/agent.log"
+LOG_FILE = "/home/opvis/utils/log/settled_monitor.log"
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 fh = TimedRotatingFileHandler(LOG_FILE,when='D',interval=1,backupCount=30)
@@ -33,39 +33,41 @@ with open("/home/opvis/utils/agent.lock", "r") as fd:
   proxy_ip = fd.readline().split(":")[0]
 settled_post_url = "http://" + proxy_ip + ":9995" + "/fixed_point_result/"  # 返回结果给transfer
 id = shell_name.split("##")[1]
-with open(shell_path,"r") as fd:
-  shell_cmd = fd.read()
-end_time = datetime.datetime.now() + datetime.timedelta(seconds=60)
-# 脚本执行开始时间
-start_execute_time = time.time()
-sub = subprocess.Popen(shell_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-while True:
-  time.sleep(0.1)
-  if end_time <= datetime.datetime.now():
-    overtime_alarm = 2
-    sub.kill()
-    break  # 为了防止结果和错误都为空，如ls一个空目录
-  else:
-    overtime_alarm = 1
-  (stdoutput, erroutput) = sub.communicate()
-  if erroutput:
-    result = erroutput
-  else:
-    result = stdoutput
-  if result:
-    break
-  if sub.poll() is not None:
-    break
-#logging.info("shell脚本定时执行结果：" + str(result))
-end_execute_time = time.time()
-data = {}
-data["id"] = id
-data["cost_time"] = round(end_execute_time - start_execute_time,3)
-data["result"] = result
-data = urllib.urlencode(data)
-req = urllib2.Request(url=settled_post_url, data=data)
-res = urllib2.urlopen(req)
-get_data = res.read()
-logging.info("transfer feedback " + str(get_data))
-
-
+# 添加一个判断，判断shell_path是否存在，如果不存在就记录日志。
+if os.path.exists(shell_path):
+  with open(shell_path, "r") as fd:
+    shell_cmd = fd.read()
+  end_time = datetime.datetime.now() + datetime.timedelta(seconds=60)
+  # 脚本执行开始时间
+  start_execute_time = time.time()
+  sub = subprocess.Popen(shell_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  while True:
+    time.sleep(0.1)
+    if end_time <= datetime.datetime.now():
+      overtime_alarm = 2
+      sub.kill()
+      break  # 为了防止结果和错误都为空，如ls一个空目录
+    else:
+      overtime_alarm = 1
+    (stdoutput, erroutput) = sub.communicate()
+    if erroutput:
+      result = erroutput
+    else:
+      result = stdoutput
+    if result:
+      break
+    if sub.poll() is not None:
+      break
+  # logging.info("shell脚本定时执行结果：" + str(result))
+  end_execute_time = time.time()
+  data = {}
+  data["id"] = id
+  data["cost_time"] = round(end_execute_time - start_execute_time, 3)
+  data["result"] = result
+  data = urllib.urlencode(data)
+  req = urllib2.Request(url=settled_post_url, data=data)
+  res = urllib2.urlopen(req)
+  get_data = res.read()
+  logging.info("transfer feedback " + str(get_data))
+else:
+  logging.info("shell_scripts下的 " + str(shell_name) + " 不存在！")
