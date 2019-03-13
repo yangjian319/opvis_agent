30a7396206534596f27bccae7ce499b2b6f076ff
yangjian319 <yangjian319@gmial.com>
2019-03-13 16:37:42 +0800

    更改升级shell

--- agent_service/update/agentupdate.sh
+++ agent_service/update/agentupdate.sh
@@ -3,6 +3,6 @@ time=`date +%Y%m%d%H%M%S`
 ps aux|grep agent_udp|grep -v grep|awk '{print $2}'|xargs kill -9
 rm -rf  /home/opvis/opvis_agent
 tar zxf /home/opvis/opvis_agent_v*.tar.gz -C /home/opvis/
-mv /home/opvis/opvis_agent_v*.tar.gz /home/opvis/opvis_agent_v*.tar.gz.$time
+mv /home/opvis/opvis_agent_v*.tar.gz /home/opvis/$1.$time
 cd /home/opvis/opvis_agent/agent_service
 sh start.sh
