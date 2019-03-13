49fb9f1312235665b2bce952067fa9924937a312
yangjian319 <yangjian319@gmial.com>
2019-03-13 09:38:43 +0800

    去掉.py

--- agent_service/update/agentupdate.sh
+++ agent_service/update/agentupdate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 time=`date +%Y%m%d%H%M%S`
-ps aux|grep agent_udp.py|grep -v grep|awk '{print $2}'|xargs kill -9
+ps aux|grep agent_udp|grep -v grep|awk '{print $2}'|xargs kill -9
 rm -rf  /home/opvis/opvis_agent
 tar zxf /home/opvis/opvis_agent_v*.tar.gz -C /home/opvis/
 mv /home/opvis/opvis_agent_v*.tar.gz /home/opvis/opvis_agent_v*.tar.gz.$time
 cd /home/opvis/opvis_agent/agent_service
-./start.sh
+sh start.sh
