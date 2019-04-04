382dd1baa90b237b4d87411fffca50f1b1dc2e63
yangjian319 <yangjian319@gmial.com>
2019-04-04 13:55:19 +0800

    增加kill端口9997

--- agent_service/reload.sh
+++ agent_service/reload.sh
@@ -1,2 +1,3 @@
-ps -ef | grep -v grep | grep agent_udp| awk '{print $2}'|xargs kill -9
+ps -ef | grep -v grep | grep agent_udp| awk '{print $2}'|xargs kill -9 >/dev/null 2>&1
+netstat -ntulp|grep 9997|grep -v grep|awk -F ' ' '{print $6}'|awk -F '/' '{print $1}'|xargs kill -9 >/dev/null 2>&1
 /usr/bin/nohup /home/opvis/opvis_agent/agent_service/agent_udp >> /home/opvis/utils/log/agent.log 2>&1 &
