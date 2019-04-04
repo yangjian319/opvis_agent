f8e6a48fe4a3fb4cbd5bf3c3151f61efb91b62c4
yangjian319 <yangjian319@gmial.com>
2019-04-04 13:57:14 +0800

    增加kill端口9997

--- agent_service/stop.sh
+++ agent_service/stop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 ps -ef | grep -v grep | grep agent_udp| awk '{print $2}'|xargs kill -9
+netstat -ntulp|grep 9997|grep -v grep|awk -F ' ' '{print $6}'|awk -F '/' '{print $1}'|xargs kill -9 >/dev/null 2>&1
